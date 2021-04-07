84199816e15da1a4f9ae3358129cf1871feacf44
ASUS <s160418030@student.ubaya.ac.id>
2021-04-07 19:30:00 +0700

    MVVM

--- app/src/main/java/id/ac/ubaya/informatika/adv160418030uts/View/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/adv160418030uts/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.ac.ubaya.informatika.adv160418030uts
+package id.ac.ubaya.informatika.adv160418030uts.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.ubaya.informatika.adv160418030uts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
